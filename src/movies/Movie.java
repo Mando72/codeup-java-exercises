1f1903140dd8a9b4a8218cc94ae6522799d58426
Armando Sifuentes <armandosifuentesjr45@gmail.com>
2018-08-03 14:58:11 -0500

    finished up and added a rating method to Movie and MoviesArray

--- src/movies/Movie.java
+++ src/movies/Movie.java
@@ -4,30 +4,49 @@ public class Movie {
 
     private String name;
     private String category;
+    private int rating;
 
     public Movie (String title, String cat){
         this.name = title;
         this.category = cat;
     }
 
+    public Movie(int rating) {
+        this.rating = rating;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
 
     public String getName(){
+
         return this.name;
     }
 
     public void setName(String name) {
+
         this.name = name;
     }
 
     public Movie(String category) {
+
         this.category = category;
     }
 
     public String getCategory() {
+
         return category;
     }
 
     public void setCategory(String category) {
+
         this.category = category;
     }
+
+    public static Movie[] movieFinder = MoviesArray.findAll();
 }
