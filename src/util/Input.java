5208f8e82958cd2be2a51c55e4df821211794e39
Armando Sifuentes <armandosifuentesjr45@gmail.com>
2018-08-07 09:05:11 -0500

    added try and catch

--- src/util/Input.java
+++ src/util/Input.java
@@ -9,53 +9,71 @@ public class Input {
     // Original version with params
     public String getString(String prompt){
         System.out.println(prompt);
-        return this.sc.nextLine().toLowerCase().trim();
+        return getString();
     }
 
     // Another version without params
     public String getString(){
-        return this.getString("Type: ");
+        return this.sc.nextLine().toLowerCase().trim();
     }
 
-    // YesNo reuses some code from getString
+    // Yes/No reuses some code from getString
     public boolean yesNo(){
         String result = this.getString("Type yes or no (y/n)");
         return (result.equals("y") || result.equals("yes"));
     }
 
     public int getInt(int min, int max){
-
         int userInput = Integer.parseInt(this.getString("Give me a number between " + min + " and " + max));
-
         if(userInput < min || userInput > max){
             System.err.println("Number our of range");
             getInt(min, max);
         }
-
         return userInput;
     }
 
+    public int getInt(String prompt){
+         System.out.println(prompt);
+         return getInt();
+    }
+
     public int getInt(){
-        return Integer.parseInt(this.getString("Give me a number"));
+        try {
+            return Integer.parseInt(this.getString("Give me a number"));
+        }catch(NumberFormatException e){
+            System.out.println("Not a number, try again...");
+            return getInt();
+        }
     }
 
     public double getDouble(double min, double max){
         double userInput = Double.parseDouble(this.getString("Give me a decimal number between " + min + " and " + max));
-
         if(userInput < min || userInput > max){
             System.err.println("Decimal number out of range");
             userInput = getDouble(min, max);
         }
-
         return userInput;
     }
 
-    public double getDouble(String s) {
-        return 0;
+        //getDouble("enter a number with decimal?")
+        //sout
+        //return method()
+
+    public double getDouble(String prompt) {
+        System.out.println(prompt);
+         return getDouble();
     }
 
     public double getDouble(){
-        return Double.parseDouble(this.getString("Give me a decimal number"));
+        //return a number
+        // = "a"?
+        //NumberFormatException
+        try{
+            return Double.parseDouble(this.getString("Give me a decimal number"));
+        }catch(NumberFormatException e){
+            System.out.println("Not a number, try again...");
+            return getDouble();
+        }
     }
 
 }
--- src/util/InputTest.java
+++ src/util/InputTest.java
@@ -4,11 +4,24 @@ public class InputTest {
     public static void main(String[] args) {
 
         Input input = new Input();
+        // Input.java is now input = new Input.java()
 
-        System.out.println(input.getString());
+        // System.out.println(input.getString());
 
-        System.out.println(input.getString("GIVE ME SOMETHING"));
+        // System.out.println(input.getString("GIVE ME SOMETHING"));
 
-        System.out.println(input.yesNo());
+        // System.out.println(input.yesNo());
+
+        // System.out.println("Your double is -> " + input.getDouble("Enter a number?"));
+        //"Your double is -> input"
+        System.out.println("test");
+        // "test" = ['t', 'e', 's', 't'] = -> TEST
     }
-}
+
+} 
+// public int getInt(int x){
+//     return x*x;
+// }
+// public double getInt(double x){
+//     return x+x;
+// }
